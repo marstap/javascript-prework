3c6d252383aa70bb96d2f6e8574bd5bf8b4bd875
Marcin Staporski <marstap@wp.pl>
2020-01-10 09:42:17 +0100

    add promt

---
+++ js/functions.js
@@ -0,0 +1,33 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+else if(randomNumber == 2) {
+	computerMove = 'papier'
+}
+else if (randomNumber == 3) {
+	computerMove = 'norzyce'
+}
+printMessage('Mój ruch to: ' + computerMove);
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+else if(randomNumber == 2) {
+	computerMove = 'papier'
+}
+else if (randomNumber == 3) {
+	computerMove = 'norzyce'
+}
