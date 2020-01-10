1bad6c168149d6d8a617888bde2598a377d3b055
Marcin Staporski <marstap@wp.pl>
2020-01-10 13:05:34 +0100

    pierwsze zadanie

--- js/functions.js
+++ js/functions.js
@@ -20,14 +20,51 @@ console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = 'nieznany ruch';
 
-if(playerInput == '1'){
+if(playerInput == '1') {
   playerMove = 'kamień';
 }
+printMessage('Twój ruch to: ' + playerInput);
 
-printMessage('Twój ruch to: ' + playerMove);
-else if(randomNumber == 2) {
-	computerMove = 'papier'
+
+else if(playerInput == '2'){
+	playerMove = 'papier'
 }
-else if (randomNumber == 3) {
-	computerMove = 'norzyce'
+  printMessage('Twój ruch to: ' + playerInput);
+
+else if (playerInput == '3'){
+	playerMove = 'norzyce'
+}
+  printMessage ('Twój ruch to: ' + playerInput);
+
+if(computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywsz');
+}
+
+else if(computerMove == 'kameień' && playerMove == "norzyce"){
+  printMessage('Komputer wygrywa');
+}
+else if(computerMove == 'kamień' && playerMove == 'norzyce') {
+    printMessage('Remis');
+}
+
+  if(computerMove == 'papier' && playerMove == 'norzyce'){
+    printMessage('Ty wygrywsz');
+}
+  else if(computerMove == 'papier' && playerMove == 'kameień'){
+    printMessage('Komputer wygrywa');
+}
+  else if(computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis');
+}
+if(computerMove == 'norzyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywsz');
+}
+  else if(computerMove == 'norzyce' && playerMove == 'papier'){
+  printMessage('Komputer wygrywa');
+}
+else if(computerMove == 'norzyce' && playerMove == 'norzyce'){
+  printMessage('Remis');
+}
+else {
+  printMessage('nieznany ruch')
 }
